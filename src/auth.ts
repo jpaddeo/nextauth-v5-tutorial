24ce3d82b61ff53ebeafc9839e3e0f50b724cb20
Juan Pablo Addeo <jpaddeo@gmail.com>
2024-01-07 21:27:35 -0300

    feat: :zap: adding auth callbacks to extend  user and token data

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, UserRole } from '@prisma/client';
+
 import authConfig from '@/auth.config';
+import { userSvc } from '@/services/user';
 
 const prisma = new PrismaClient();
 
@@ -13,5 +15,30 @@ export const {
 } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
+  callbacks: {
+    async signIn({ user }) {
+      // const existingUser = await userSvc.userById(user.id);
+      // if (!existingUser || !existingUser?.emailVerified || !existingUser.active) return false;
+      return true;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await userSvc.userById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+  },
   ...authConfig,
 });
